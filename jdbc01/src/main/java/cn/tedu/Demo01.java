666d165b6dd2546181b38716646794d77887e704
Xiloer <83123095@qq.com>
2022-03-15 14:25:02 +0800

    Changes

--- jdbc01/src/main/java/cn/tedu/Demo01.java
+++ jdbc01/src/main/java/cn/tedu/Demo01.java
@@ -3,6 +3,7 @@ package cn.tedu;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Demo01 {
     public static void main(String[] args) throws SQLException {
@@ -12,9 +13,12 @@ public class Demo01 {
                 DriverManager.getConnection("jdbc:mysql://localhost:3306/empdb?characterEncoding=utf8&serverTimezone=Asia/Shanghai",
                         "root","root");
         System.out.println("连接对象:"+conn);
-
         //2. 创建执行SQL语句的对象
-        //3. 执行SQL语句
+        Statement s = conn.createStatement();
+        //3. 执行SQL语句 execute执行
+        s.execute("create table jdbct1(name varchar(20))");
         //4. 关闭连接
+        conn.close();
+        System.out.println("创建完成!");
     }
 }
