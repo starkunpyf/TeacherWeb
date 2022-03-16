c048a05a266e78bad8ed53e5ac77629c5c765bf7
ivan <77331283@qq.com>
2022-03-16 14:47:12 +0800

    Changes

---
+++ boot1-1/src/test/java/cn/tedu/boot11/Boot11ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- jdbc03/src/main/java/cn/tedu/Demo06.java
+++ jdbc03/src/main/java/cn/tedu/Demo06.java
@@ -41,3 +41,9 @@ public class Demo06 {
         }
     }
 }
+
+
+
+
+
+
