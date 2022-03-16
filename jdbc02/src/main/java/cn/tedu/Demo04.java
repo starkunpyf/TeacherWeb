b408460579b9eeddb4f6e6765403348ffbdf9f4d
Xiloer <83123095@qq.com>
2022-03-16 09:03:54 +0800

    Changes

---
+++ jdbc02/src/main/java/cn/tedu/Demo04.java
@@ -0,0 +1,21 @@
+package cn.tedu;
+
+public class Demo04 {
+    public static void main(String[] args) {
+//        String name = "张三";
+//        int age = 18;
+//        String info = "我叫"+name+"今年"+age+"岁!";
+        String name = "孙悟空";
+        double money = 500;
+        String sql = "insert into hero values(null,'"+name+"',"+money+")";
+
+
+
+
+
+
+
+
+
+    }
+}
