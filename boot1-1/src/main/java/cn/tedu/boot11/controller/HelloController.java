36e9a3a12eb889767979f7c3f1d122ac8435af9f
ivan <77331283@qq.com>
2022-03-16 16:00:18 +0800

    Changes

--- boot1-1/src/main/java/cn/tedu/boot11/controller/HelloController.java
+++ boot1-1/src/main/java/cn/tedu/boot11/controller/HelloController.java
@@ -10,18 +10,20 @@ import java.io.PrintWriter;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/hello") // http://localhost:8080/hello
-    public void hello(HttpServletResponse response) throws IOException {
-        System.out.println("服务器接收到请求");
-        //设置响应类型 告诉客户端浏览器响应的内容是文本或标签 字符集utf-8
-        response.setContentType("text/html;charset=utf-8");
-        //获取输出对象 异常抛出
-        PrintWriter pw = response.getWriter();
-        //给客户端响应数据
-        pw.print("恭喜你测试成功!");
-        pw.close();//关闭资源
+//    @RequestMapping("/hello") // http://localhost:8080/hello
+//    public void hello(HttpServletResponse response) throws IOException {
+//        System.out.println("服务器接收到请求");
+//        //设置响应类型 告诉客户端浏览器响应的内容是文本或标签 字符集utf-8
+//        response.setContentType("text/html;charset=utf-8");
+//        //获取输出对象 异常抛出
+//        PrintWriter pw = response.getWriter();
+//        //给客户端响应数据
+//        pw.print("恭喜你测试成功!");
+//        pw.close();//关闭资源
+//    }
+    @RequestMapping("/hello")
+    @ResponseBody //此注解作用: 可以通过返回值的方式给客户端响应数据
+    public String hello(){
+        return "恭喜你又测试成功了!";
     }
-
-
-
 }
