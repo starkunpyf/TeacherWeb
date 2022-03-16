ad04833d7a796c01c1a9caaf4b0cf2677117fbf3
ivan <77331283@qq.com>
2022-03-16 16:44:07 +0800

    Changes

---
+++ boot1-1/src/main/java/cn/tedu/boot11/controller/ParamController.java
@@ -0,0 +1,18 @@
+package cn.tedu.boot11.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ParamController {
+    @RequestMapping("/param1")
+    @ResponseBody
+    public String param1(HttpServletRequest request){
+        //获取传递过来的参数
+        String info = request.getParameter("info");
+        return "测试成功! info="+info;
+    }
+}
