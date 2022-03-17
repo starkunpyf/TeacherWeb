309f00aa664c0908b80f6275b5dcec79e7b255cb
ivan <77331283@qq.com>
2022-03-17 10:25:41 +0800

    Changes

---
+++ boot2-1/src/main/java/cn/tedu/boot21/controller/UserController.java
@@ -0,0 +1,18 @@
+package cn.tedu.boot21.controller;
+
+import cn.tedu.boot21.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+//相当于在Controller注解基础上 给所有方法都添加了 @ResponseBody注解
+@RestController
+public class UserController {
+    @RequestMapping("/reg")
+    public String reg(User user){
+        System.out.println("user = " + user);
+        return "注册成功!";
+    }
+
+
+}
---
+++ boot2-1/src/main/java/cn/tedu/boot21/entity/User.java
@@ -0,0 +1,40 @@
+package cn.tedu.boot21.entity;
+
+public class User {
+    private String username;
+    private String password;
+    private String nick;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nick='" + nick + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
