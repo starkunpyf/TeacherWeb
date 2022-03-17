5126c18be6b1f5f081eb7f584840e119afc6bf7a
ivan <77331283@qq.com>
2022-03-17 14:24:18 +0800

    Changes

--- boot2-2/src/main/java/cn/tedu/boot22/controller/EmpController.java
+++ boot2-2/src/main/java/cn/tedu/boot22/controller/EmpController.java
@@ -5,9 +5,8 @@ import cn.tedu.boot22.utils.DBUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 @RestController
 public class EmpController {
@@ -32,12 +31,42 @@ public class EmpController {
     @RequestMapping("/select")
     public String select(){
         //获取连接 查询所有员工信息
-
+        ArrayList<Emp> list = new ArrayList<>();
+        try (Connection conn = DBUtils.getConn()){
+            String sql = "select * from myemp";
+            Statement s = conn.createStatement();
+          //执行查询
+            ResultSet rs = s.executeQuery(sql);
+            //遍历结果集对象
+            while(rs.next()){
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                int sal = rs.getInt(3);
+                String job = rs.getString(4);
+                //把员工信息封装到实例类中
+                Emp e = new Emp();
+                e.setId(id);
+                e.setName(name);
+                e.setSal(sal);
+                e.setJob(job);
+                //把员工对象装进list集合
+                list.add(e);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         String html = "<table border=1>";
         html+="<caption>员工列表</caption>";
         html+="<tr><th>id</th><th>名字</th><th>工资</th><th>工作</th></tr>";
         //把查询出来的员工信息添加到表格中
-
+        for (Emp e:list) {
+            html+="<tr>";
+            html+="<td>"+e.getId()+"</td>";
+            html+="<td>"+e.getName()+"</td>";
+            html+="<td>"+e.getSal()+"</td>";
+            html+="<td>"+e.getJob()+"</td>";
+            html+="</tr>";
+        }
         html+="</table>";
         return html;
     }
