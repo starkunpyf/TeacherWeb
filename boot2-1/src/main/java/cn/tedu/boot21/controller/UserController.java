e2effd6b5d7dc6382a18f6c30bbc40b8145bc75e
ivan <77331283@qq.com>
2022-03-17 11:00:14 +0800

    Changes

--- boot2-1/src/main/java/cn/tedu/boot21/controller/UserController.java
+++ boot2-1/src/main/java/cn/tedu/boot21/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 //相当于在Controller注解基础上 给所有方法都添加了 @ResponseBody注解
@@ -29,5 +30,28 @@ public class UserController {
             throwables.printStackTrace();
         }
         return "注册成功!";
+    }
+
+    @RequestMapping("/login")
+    public String login(User user){
+        System.out.println("user = " + user);
+        //获取数据库连接
+        try (Connection conn = DBUtils.getConn()){
+            //准备通过用户名查询密码的SQL语句
+            String sql = "select password from user where username=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            //替换?为用户名
+            ps.setString(1,user.getUsername());
+            ResultSet rs = ps.executeQuery();
+            //判断是否查询到了内容
+            if (rs.next()){
+                //得到查询回来的正确密码
+                String pw = rs.getString(1);
+                //拿用户输入的密码和正确密码判断是否一样
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "登录成功!";
     }
 }
