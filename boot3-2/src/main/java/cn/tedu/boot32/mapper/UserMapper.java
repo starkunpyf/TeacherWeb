3f97eead22b18bbd7fec2b9b29eeb637d4f8c99d
ivan <77331283@qq.com>
2022-03-18 14:19:09 +0800

    Changes

---
+++ boot3-2/src/main/java/cn/tedu/boot32/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.tedu.boot32.mapper;
+
+import cn.tedu.boot32.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    //注册方法
+    @Insert("insert into user values(null,#{username},#{password},#{nick})")
+    void insert(User user);
+
+    //登录方法 通过用户名查询到密码 并且把密码封装到了User对象里面
+    @Select("select password from user where username=#{username}")
+    User selectByUsername(String username);
+}
---
+++ boot3-2/src/test/java/cn/tedu/boot32/Boot32ApplicationTests.java
@@ -0,0 +1,23 @@
+package cn.tedu.boot32;
+
+import cn.tedu.boot32.entity.User;
+import cn.tedu.boot32.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot32ApplicationTests {
+    @Autowired(required = false)
+    UserMapper mapper;
+
+    @Test
+    void contextLoads() {
+        User u = new User();
+        u.setUsername("liudehua");
+        u.setPassword("aabbcc");
+        u.setNick("刘德华");
+        mapper.insert(u);
+    }
+
+}
