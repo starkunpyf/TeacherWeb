d5d6980142b57ed32faf3b9a580d2ba95fcae4a6
ivan <77331283@qq.com>
2022-03-18 15:11:33 +0800

    Changes

--- boot3-2/src/main/java/cn/tedu/boot32/controller/UserController.java
+++ boot3-2/src/main/java/cn/tedu/boot32/controller/UserController.java
@@ -13,6 +13,13 @@ public class UserController {
     @RequestMapping("/reg")
     public String reg(User user){
         System.out.println("user = " + user);
-        return "";
+        //通过用户输入的用户名查询是否存在数据  u代表查询回来的用户信息
+        User u = mapper.selectByUsername(user.getUsername());
+        if (u!=null){ //如果不为null 说明此用户名已存在
+            return "用户名已存在!";
+        }
+        //用户名不存在 可以注册
+        mapper.insert(user);
+        return "注册成功!";
     }
 }
