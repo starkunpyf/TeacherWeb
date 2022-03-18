832b9cb57f7bd5e2fa26b336cf53a8b4e76b574b
ivan <77331283@qq.com>
2022-03-18 15:51:33 +0800

    Changes

---
+++ boot3-3/src/main/java/cn/tedu/boot33/Boot33Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot33Application.class, args);
+    }
+
+}
