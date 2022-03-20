b07614a70d6f341973e1a87e21feebd7eb52bc14
ivan <77331283@qq.com>
2022-03-20 09:49:43 +0800

    Changes

---
+++ boot4-0/src/main/java/cn/tedu/boot40/Boot40Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot40Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot40Application.class, args);
+    }
+
+}
---
+++ boot4-1/src/main/java/cn/tedu/boot41/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package cn.tedu.boot41.mapper;
+
+import cn.tedu.boot41.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Select("select password from user where username=#{username}")
+    User selectByUsername(String username);
+    @Insert("insert into user values(null,#{username},#{password},#{nick})")
+    void insert(User user);
+
+}
