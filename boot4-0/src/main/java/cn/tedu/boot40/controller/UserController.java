f6695b04ee1e3fdea651687ffbd6330fe4af56dd
ivan <77331283@qq.com>
2022-03-20 09:32:19 +0800

    Changes

---
+++ boot4-0/src/main/java/cn/tedu/boot40/controller/UserController.java
@@ -0,0 +1,35 @@
+package cn.tedu.boot40.controller;
+
+import cn.tedu.boot40.entity.User;
+import cn.tedu.boot40.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired(required = false)
+    UserMapper mapper;
+    @RequestMapping("/reg")
+    public String reg(User user){
+
+        User u = mapper.selectByUsername(user.getUsername());
+        if (u!=null){
+            return "用户名已存在!";
+        }
+        mapper.insert(user);
+
+        return "注册成功!";
+    }
+    @RequestMapping("/login")
+    public String login(User user){
+        User u = mapper.selectByUsername(user.getUsername());
+        if (u!=null){
+            if (u.getPassword().equals(user.getPassword())){
+                return "登录成功!";
+            }
+            return "密码错误!";
+        }
+        return "用户名不存在!";
+    }
+}
