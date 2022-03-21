76a9e85c36f46aa6171ad2eca05cba659da3ffaf
ivan <77331283@qq.com>
2022-03-21 15:40:49 +0800

    Changes

--- boot4-222/src/main/java/cn/tedu/boot4222/controller/EmpController.java
+++ boot4-222/src/main/java/cn/tedu/boot4222/controller/EmpController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmpController {
     @Autowired(required = false)
@@ -15,5 +17,13 @@ public class EmpController {
     public void insert(Emp emp){
         System.out.println("emp = " + emp);
         mapper.insert(emp);
+    }
+    @RequestMapping("/select")
+    public List<Emp> select(){
+
+        //当方法return的类型为自定义对象或集合时,
+        // SpringMVC框架会自动将集合或对象转成Json格式的字符串
+        //然后将字符串再转成二进制数据通过网络传输给客户端
+        return mapper.select();
     }
 }
