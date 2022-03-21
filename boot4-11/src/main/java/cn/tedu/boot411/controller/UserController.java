bb9880eadade6ed6ca85080ff84f076391566f5d
ivan <77331283@qq.com>
2022-03-21 10:55:57 +0800

    Changes

--- boot4-11/src/main/java/cn/tedu/boot411/controller/UserController.java
+++ boot4-11/src/main/java/cn/tedu/boot411/controller/UserController.java
@@ -1,16 +1,23 @@
 package cn.tedu.boot411.controller;
 
 import cn.tedu.boot411.entity.User;
+import cn.tedu.boot411.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired(required = false)
+    UserMapper mapper;
 
     @RequestMapping("/reg")
-    public String reg(User user){
-
-
-        return "注册成功!";
+    public int reg(User user){
+        User u = mapper.selectByUsername(user.getUsername());
+        if (u!=null){
+            return 2; //代表用户名已存在
+        }
+        mapper.insert(user);
+        return 1; //代表注册成功
     }
 }
