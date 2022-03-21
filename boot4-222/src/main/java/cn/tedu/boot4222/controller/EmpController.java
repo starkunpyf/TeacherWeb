8abeac752cf66523e34993cb65eeb7752992fa91
ivan <77331283@qq.com>
2022-03-21 14:40:23 +0800

    Changes

--- boot4-222/src/main/java/cn/tedu/boot4222/controller/EmpController.java
+++ boot4-222/src/main/java/cn/tedu/boot4222/controller/EmpController.java
@@ -1,13 +1,19 @@
 package cn.tedu.boot4222.controller;
 
 import cn.tedu.boot4222.entity.Emp;
+import cn.tedu.boot4222.mapper.EmpMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmpController {
+    @Autowired(required = false)
+    EmpMapper mapper;
+
     @RequestMapping("/insert")
     public void insert(Emp emp){
         System.out.println("emp = " + emp);
+        mapper.insert(emp);
     }
 }
