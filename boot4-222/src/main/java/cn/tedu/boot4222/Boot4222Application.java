f5f8e5c3f9f0105904e38a2119d15ebef0f97078
ivan <77331283@qq.com>
2022-03-21 14:21:44 +0800

    Changes

---
+++ boot4-222/src/main/java/cn/tedu/boot4222/Boot4222Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot4222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot4222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot4222Application.class, args);
+    }
+
+}
