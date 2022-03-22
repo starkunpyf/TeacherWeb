1389f4a6494ede2718348340615251f23e1f050a
ivan <77331283@qq.com>
2022-03-22 17:45:32 +0800

    Changes

--- boot5-2/src/main/java/cn/tedu/boot52/controller/UserController.java
+++ boot5-2/src/main/java/cn/tedu/boot52/controller/UserController.java
@@ -7,18 +7,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class UserController {
     @Autowired(required = false)
     UserMapper mapper;
 
     @RequestMapping("/login")
-    public int login(@RequestBody User user){
+    public int login(@RequestBody User user, HttpServletResponse response){
         System.out.println("user = " + user);
 
         User u = mapper.selectByUsername(user.getUsername());
         if (u!=null){
             if (u.getPassword().equals(user.getPassword())){
+                //判断是否需要记住用户名和密码
+                if (user.isRem()){
+                    //创建Cookie记住用户名和密码
+                    Cookie c1 = new Cookie("username",user.getUsername());
+                    Cookie c2 = new Cookie("password",user.getPassword());
+                    //把cookie下发给客户端
+                    response.addCookie(c1);
+                    response.addCookie(c2);
+                }
                 return 1;
             }
             return 3;
