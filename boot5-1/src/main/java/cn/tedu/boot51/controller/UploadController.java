694054187213755b20964242a8a5e905e014e41c
ivan <77331283@qq.com>
2022-03-22 10:39:27 +0800

    Changes

--- boot5-1/src/main/java/cn/tedu/boot51/controller/UploadController.java
+++ boot5-1/src/main/java/cn/tedu/boot51/controller/UploadController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.UUID;
 
 @RestController
@@ -18,7 +19,13 @@ public class UploadController {
         //得到唯一文件名 UUID.randomUUID()获取唯一标识符
         fileName = UUID.randomUUID()+suffix;
         System.out.println("文件名:"+fileName);
-
+        //保存图片的文件夹
+        String dirPath = "D:/file";
+        File dirFile = new File(dirPath);
+        //判断如果文件夹不存在 则创建文件夹
+        if (!dirFile.exists()){
+            dirFile.mkdirs();//创建文件夹
+        }
 
         return "测试上传";
     }
