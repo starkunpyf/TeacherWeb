09edf2fe08564bcba897f29c05dae69c8b16a71d
ivan <77331283@qq.com>
2022-03-22 16:55:00 +0800

    Changes

---
+++ boot5-2/src/main/java/cn/tedu/boot52/Boot52Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot52Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot52Application.class, args);
+    }
+
+}
---
+++ boot5-2/src/test/java/cn/tedu/boot52/Boot52ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot52ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
