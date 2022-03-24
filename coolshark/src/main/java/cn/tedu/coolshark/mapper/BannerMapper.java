c53444d771c7004f6e982656464292f90adf640b
ivan <77331283@qq.com>
2022-03-24 14:50:11 +0800

    Changes

---
+++ coolshark/src/main/java/cn/tedu/coolshark/mapper/BannerMapper.java
@@ -0,0 +1,18 @@
+package cn.tedu.coolshark.mapper;
+
+import cn.tedu.coolshark.entity.Banner;
+import cn.tedu.coolshark.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface BannerMapper {
+    @Insert("insert into banner values(null,#{url})")
+    void insert(Banner banner);
+    @Select("select id,url from banner")
+    List<Banner> select();
+
+}
