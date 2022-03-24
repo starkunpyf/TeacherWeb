d4b3dc33abb1e0ac8dc590ef047a2467290bd14b
ivan <77331283@qq.com>
2022-03-24 10:01:56 +0800

    Changes

---
+++ boot7-0weibo/src/main/java/cn/tedu/boot70weibo/Boot70weiboApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot70weibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot70weiboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot70weiboApplication.class, args);
+    }
+
+}
---
+++ boot7-0weibo/src/test/java/cn/tedu/boot70weibo/Boot70weiboApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot70weibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot70weiboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
