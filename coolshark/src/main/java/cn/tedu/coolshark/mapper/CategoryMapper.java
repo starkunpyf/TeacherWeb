5242bb80d26a6db49b930aa9db293074a9d9fa74
ivan <77331283@qq.com>
2022-03-24 14:19:35 +0800

    Changes

---
+++ coolshark/src/main/java/cn/tedu/coolshark/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package cn.tedu.coolshark.mapper;
+
+import cn.tedu.coolshark.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+    @Insert("insert into category values(null,#{name})")
+    void insert(Category category);
+    @Select("select id,name from category")
+    List<Category> select();
+}
