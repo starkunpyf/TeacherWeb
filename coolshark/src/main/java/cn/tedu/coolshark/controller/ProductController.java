37ba230e033049c4e61bda5f0bfe8e5dafd8005f
ivan <77331283@qq.com>
2022-03-25 16:03:46 +0800

    Changes

---
+++ coolshark/src/main/java/cn/tedu/coolshark/controller/ProductController.java
@@ -0,0 +1,13 @@
+package cn.tedu.coolshark.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @RequestMapping("/product/insert")
+    public void insert(){
+
+    }
+}
---
+++ coolshark/src/main/java/cn/tedu/coolshark/entity/Product.java
@@ -0,0 +1,102 @@
+package cn.tedu.coolshark.entity;
+
+import java.util.Date;
+
+public class Product {
+    private Integer id;
+    private String title;
+    private String url;
+    private Double price;
+    private Double oldPrice;
+    private Integer viewCount; //浏览量
+    private Integer saleCount;
+    private Date   created;  //发布时间
+    private Integer categoryId; //商品分类id
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", price=" + price +
+                ", oldPrice=" + oldPrice +
+                ", viewCount=" + viewCount +
+                ", saleCount=" + saleCount +
+                ", created=" + created +
+                ", categoryId=" + categoryId +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(Double oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getSaleCount() {
+        return saleCount;
+    }
+
+    public void setSaleCount(Integer saleCount) {
+        this.saleCount = saleCount;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
