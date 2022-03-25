154ce593f79ec85484560e7955e4d39722540b1d
ivan <77331283@qq.com>
2022-03-25 16:38:24 +0800

    Changes

--- coolshark/src/main/java/cn/tedu/coolshark/controller/ProductController.java
+++ coolshark/src/main/java/cn/tedu/coolshark/controller/ProductController.java
@@ -1,13 +1,24 @@
 package cn.tedu.coolshark.controller;
 
+import cn.tedu.coolshark.entity.Product;
+import cn.tedu.coolshark.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class ProductController {
+    @Autowired(required = false)
+    ProductMapper mapper;
 
     @RequestMapping("/product/insert")
-    public void insert(){
+    public void insert(@RequestBody Product product){
+        System.out.println("product = " + product);
+        product.setCreated(new Date());
 
+        mapper.insert(product);
     }
 }
